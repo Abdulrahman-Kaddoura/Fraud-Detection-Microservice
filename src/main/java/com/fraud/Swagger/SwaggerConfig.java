258d52ed4cb0024370587b0b92354698d7e0a8db
abdkad123 <akaddoura123@gmail.com>
2026-01-02 18:03:51 +0200

    feat: configured swagger

--- src/main/java/com/fraud/Swagger/SwaggerConfig.java
+++ src/main/java/com/fraud/Swagger/SwaggerConfig.java
@@ -1,4 +1,21 @@
 package com.fraud.Swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI().info(new Info().title("fraud API's").description("API documentation").version("1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder().group("public").packagesToScan("com.fraud").build();
+    }
 }
